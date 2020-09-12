fe5e9564ac24f91027ab529a422ab5cdfefe99af
Mohamed Elsaed <ashraf@orcas.io>
2020-09-12 13:13:41 +0200

    add to footer and head

--- resources/views/layouts/WebsiteLayouts/footer.blade.php
+++ resources/views/layouts/WebsiteLayouts/footer.blade.php
@@ -95,24 +95,15 @@
         </div>
     </div>
 </footer>
-<!--::footer_part end::-->
-
-<!-- jquery plugins here-->
 <script src="{{asset('js/jquery-1.12.1.min.js')}}"></script>
-<!-- popper js -->
 <script src="{{asset('js/popper.min.js')}}"></script>
-<!-- bootstrap js -->
 <script src="{{asset('js/bootstrap.min.js')}}"></script>
-<!-- easing js -->
 <script src="{{asset('js/jquery.magnific-popup.js')}}"></script>
-<!-- swiper js -->
+<script src="{{asset('js/lightslider.min.js')}}"></script>
 <script src="{{asset('js/swiper.min.js')}}"></script>
-<!-- swiper js -->
 <script src="{{asset('js/masonry.pkgd.js')}}"></script>
-<!-- particles js -->
 <script src="{{asset('js/owl.carousel.min.js')}}"></script>
 <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
-<!-- slick js -->
 <script src="{{asset('js/slick.min.js')}}"></script>
 <script src="{{asset('js/jquery.counterup.min.js')}}"></script>
 <script src="{{asset('js/waypoints.min.js')}}"></script>
@@ -121,8 +112,8 @@
 <script src="{{asset('js/jquery.form.js')}}"></script>
 <script src="{{asset('js/jquery.validate.min.js')}}"></script>
 <script src="{{asset('js/mail-script.js')}}"></script>
-<!-- custom js -->
-<script src="{{asset('')}}js/custom.js"></script>
+<script src="{{asset('js/theme.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
 </body>
 
 </html>
