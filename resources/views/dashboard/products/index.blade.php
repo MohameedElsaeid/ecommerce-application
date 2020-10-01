0bd7b4ad7b261c7bfca6dd01942560fe3ae8262a
Toka-Sherif <48538706+Toka-Sherif@users.noreply.github.com>
2020-10-01 16:41:11 +0200

    index products complete

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -14,29 +14,56 @@
                     <div class="card-body">
                         <table id="example2" class="table table-bordered table-hover">
                             <thead>
-                            <tr>
-                                <th>Serial</th>
-                                <th>Product Title</th>
-                                <th>Product Price</th>
-                                <th>Product Brief</th>
-                                <th>Product Description</th>
-                               
-                            </tr>
+                                <tr>
+                                    <th>Serial</th>
+                                    <th>Product Title</th>
+                                    <th>Product Price</th>
+                                    <th>Product Brief</th>
+                                    {{-- <th>Product Description</th>
+                                    --}}
+                                    <th>Product Image</th>
+                                    <th>Specifications</th>
+                                    <th>Comments</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            @foreach($products as $product)
-                                <tr>
-                                    <td style="width: 100px">{{$product->product_id}}</td>
-                                    <td>{{$product->title}}</td>
-                                    <td>{{$product->price}}</td>
-                                    <td>{{$product->brief}}</td>
-                                    <td>{{$product->description}}</td>
-                                    
-                                    <td>
-                                      {{--   <button><a href="{{route('brand.edit',[$brand->$brand_id])}}">Edit</a></button> --}}
-                                    </td>
-                                </tr>
-                            @endforeach
+                                @foreach ($products as $product)
+                                    <tr>
+                                        <td style="width: 100px">{{ $product->product_id }}</td>
+                                        <td>{{ $product->title }}</td>
+                                        <td>{{ $product->price }}</td>
+                                        <td>{{ $product->brief }}</td>
+                                        {{-- <td>{{ $product->description }}</td>
+                                        --}}
+                                        <td>
+                                            <ul type="square">
+                                                @foreach ($product->images as $imageObject)
+                                                    <li> {{ $imageObject->image }} </li>
+                                                @endforeach
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            <ul type="square">
+                                                <li>width:{{ $product->specification->width }}</li>
+                                                <li>height:{{ $product->specification->height }}</li>
+                                                <li>depth:{{ $product->specification->depth }}</li>
+                                                <li>weight:{{ $product->specification->weight }}</li>
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            <ul type="square">
+                                                @foreach ($product->comments as $commentObject)
+                                                    <li>{{$commentObject->comment}}</li>
+                                                @endforeach
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            {{-- <button><a
+                                                    href="{{ route('brand.edit', [$brand->$brand_id]) }}">Edit</a></button>
+                                            --}}
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -59,5 +86,6 @@
             "autoWidth": true,
             "responsive": true,
         });
+
     </script>
 @endsection
