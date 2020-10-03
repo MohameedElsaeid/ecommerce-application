58d983f10a60c9b7cbe6367a3cda357b93cac6ae
Mohamed Elsaed <ashraf@orcas.io>
2020-10-03 10:36:47 +0200

    fix typo

--- app/Http/Controllers/DashboardController/ProductController.php
+++ app/Http/Controllers/DashboardController/ProductController.php
@@ -5,25 +5,30 @@ namespace App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateProductRequest;
 use App\Repositories\IProductRepository;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
-class ProductConroller extends Controller
+class ProductController extends Controller
 {
 
 
     /**
      * @var IProductRepository
      */
-    private $product;
- /**
+    private $productRepository;
+
+    /**
      * CategoryController constructor.
-     * @param IProductRepository $categoryRepository
+     * @param IProductRepository $productRepository
      */
-    
-    /* public function __construct(IProductRepository $productrepository )
+
+    public function __construct(IProductRepository $productRepository)
     {
-        $this->productrepository = $productrepository;
-    } */
+        $this->productrepository = $productRepository;
+    }
 
     /**
      * @return Application|Factory|View
@@ -31,7 +36,7 @@ class ProductConroller extends Controller
 
     public function index()
     {
-        $data['products'] = $this->product->get();
+        $data['products'] = $this->productrepository->get();
         return view('dashboard.products.index', $data);
     }
 
@@ -43,14 +48,14 @@ class ProductConroller extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
-        return view('dashboard.products.create'); 
+        return view('dashboard.products.create');
     }
 
-      /**
+    /**
      * @param CreateProductRequest $request
      * @return Application|RedirectResponse|Redirector
      */
@@ -58,14 +63,14 @@ class ProductConroller extends Controller
     {
         $this->brand->createnewProduct($request);
         return redirect(route('product.index'))->with('success', 'Product Created Successfully');
-   
+
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function show($id)
     {
@@ -75,8 +80,8 @@ class ProductConroller extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function edit($id)
     {
@@ -86,9 +91,9 @@ class ProductConroller extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param int     $id
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -98,8 +103,8 @@ class ProductConroller extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return Response
      */
     public function destroy($id)
     {
